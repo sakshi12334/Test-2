fe0d1533128726bef4aedf5500718fc9fabf3c8e
Sakshi Singh <sakshisingh27582@gmail.com>
2026-02-09 13:52:40 +0530

    feat: update sql changes

--- init.sql
+++ init.sql
@@ -8,8 +8,8 @@ CREATE TABLE InvoiceItems (
     ItemID INT PRIMARY KEY,
     InvoiceID INT,
     Name VARCHAR(100),
-    Price DECIML(10,2),
-    FOREIGN KEY (InvoiceID) REFRENCES Invoices(InvoiceID)
+    Price DECIMAL(10,2),
+    FOREIGN KEY (InvoiceID) REFERENCES Invoices(InvoiceID)
 );
 
 INSERT INTO Invoices (InvoiceID, CustomerName) VALUES (1, 'John Doe');
